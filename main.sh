ba351aa5edec24b575dabc04253512cadb46bf23
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-09-21 12:27:40 -0300

    Script selector sketch

--- main.sh
+++ main.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 
+while :;
+do
+  # Interface
+  read -p 'Select one of thirteen scripts to run. [1-13] ' option
+
+  if ! [[ option =~ '^[0-9]+$' ]]; then
+    echo Error: $option is not a number.
+    continue
+  fi
+
+  # prototype
+
+  # run='./exercises/ex'$option'.sh'
+  # cat statement.txt # | grep
+  # $run
+
+  read -p 'Continue? [Y/n] ' quit
+  case $quit in
+    n|N) exit ;;
+    *) continue ;;
+  esac
+done
+

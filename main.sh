f677b013dded101508aafa48e164f8d0c0260305
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-09-23 10:22:55 -0300

    Specific scripts' input implementation

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-while :;
-do
+while :; do
   # Exercise selection
   read -p 'Select one of thirteen scripts to run. [0=quit/1-13] ' option
 
@@ -10,21 +9,34 @@ do
       echo Exiting...
       exit
     fi
-    echo Problem $1:
+
+    echo
+    echo "Problem $option:"
     cat statements/stat$option
-    # Generic execution
-    ./exercises/ex$option'.sh'
+    echo
+
+    case $option in
+    1 | 3 | 4 | 7 | 9 | 11)
+      read -p 'Input: ' input
+      echo 'Output:'
+      ./exercises/ex$option'.sh' $input
+      ;;
+    *)
+      echo 'Output:'
+      ./exercises/ex$option'.sh'
+      ;;
+    esac
+
   else
     echo Invalid number, try again.
     continue
   fi
-  
+
   # Exit
   read -p 'Continue? [Y/n] ' quit
 
   case $quit in
-    n|N) exit ;;
-    *) continue ;;
+  n | N) exit ;;
+  *) continue ;;
   esac
 done
-

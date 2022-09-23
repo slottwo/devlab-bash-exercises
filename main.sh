c98763e07f84a40439d443129a99bc500ad774f0
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2022-09-23 08:49:57 -0300

    Exercise selection with generic implementation

--- main.sh
+++ main.sh
@@ -2,21 +2,26 @@
 
 while :;
 do
-  # Interface
-  read -p 'Select one of thirteen scripts to run. [1-13] ' option
+  # Exercise selection
+  read -p 'Select one of thirteen scripts to run. [0=quit/1-13] ' option
 
-  if ! [[ option =~ '^[0-9]+$' ]]; then
-    echo Error: $option is not a number.
+  if [[ $option =~ ^[0-9]+$ ]]; then
+    if [[ $option == 0 ]]; then
+      echo Exiting...
+      exit
+    fi
+    echo Problem $1:
+    cat statements/stat$option
+    # Generic execution
+    ./exercises/ex$option'.sh'
+  else
+    echo Invalid number, try again.
     continue
   fi
-
-  # prototype
-
-  # run='./exercises/ex'$option'.sh'
-  # cat statement.txt # | grep
-  # $run
-
+  
+  # Exit
   read -p 'Continue? [Y/n] ' quit
+
   case $quit in
     n|N) exit ;;
     *) continue ;;
